343c176330eeda1310637f4c0eb032fac98fdfab
newtef12 <newtef398@gmail.com>
2024-03-14 14:00:56 -0700

    jj

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
+    """
+    Replicates the behavior of the given bytecode.
+    """
     import magic_calculation_102
 
+    # Import add and sub functions from magic_calculation_102
+    add = magic_calculation_102.add
+    sub = magic_calculation_102.sub
+
+    # Check if a is less than b
     if a < b:
-        c = magic_calculation_102.add(a, b)
+        # Calculate c as the result of adding a and b
+        c = add(a, b)
+        # Add numbers from 4 to 5 to c
         for i in range(4, 6):
-            c = magic_calculation_102.add(c, i)
+            c = add(c, i)
         return c
     else:
-        return magic_calculation_102.sub(a, b)
+        # Return the result of subtracting b from a
+        return sub(a, b)
