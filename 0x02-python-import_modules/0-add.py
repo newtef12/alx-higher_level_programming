215e7c028bb9879fb5160c9a8d27ed6c9aa339f3
newtef12 <newtef398@gmail.com>
2024-03-15 02:49:33 -0700

    ll

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     """Print the sum of 1 and 2."""
     from add_0 import add
 
+    # Define variables a and b
     a = 1
     b = 2
+
+    # Print the addition result using string formatting
     print("{} + {} = {}".format(a, b, add(a, b)))
