91c5723df1b75a78ee62dd477795689ed07eee63
newtef12 <newtef398@gmail.com>
2024-03-15 03:23:56 -0700

    kk

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,19 +5,13 @@
  * @list: Pointer to the head of the linked list.
  * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
-int check_cycle(listint_t *list)
-{
-    listint_t *slow = list;
-    listint_t *fast = list;
-
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;          /* Move slow pointer by one step */
-        fast = fast->next->next;    /* Move fast pointer by two steps */
-
-        if (slow == fast)           /* If slow and fast pointers meet, there's a cycle */
+int check_cycle(listint_t *list) {
+    listint_t *slow = list, *fast = list;
+    while (slow && fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
             return (1);
     }
-
-    return (0);  /* No cycle found */
+    return (0);
 }
