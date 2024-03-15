c0f8467ee85c9527b307b03b1cbb76d7cd91c7b2
newtef12 <newtef398@gmail.com>
2024-03-15 03:39:53 -0700

    ii

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,5 @@
 def magic_calculation(a, b):
     result = 0                 # LOAD_CONST 0 (0)
-    result += 98               # LOAD_CONST 1 (98), BINARY_ADD
     result += (a ** b)         # LOAD_FAST a, LOAD_FAST b, BINARY_POWER, BINARY_ADD
+    result += 98               # LOAD_CONST 1 (98), BINARY_ADD
     return result              # RETURN_VALUE
