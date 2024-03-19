7c2c28bbd9cf96a9c05a162f3c778b8d02d79387
newtef12 <newtef398@gmail.com>
2024-03-19 05:48:32 -0700

    ll

--- 0x03-python-data_structures/11-main.py
+++ 0x03-python-data_structures/11-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]
-    else:
-        return my_list[:idx] + my_list[idx+1:]
+delete_at = __import__('11-delete_at').delete_at
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
