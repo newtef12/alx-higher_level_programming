160f1fccc4568f6c9ee9232883d1d9838179e2d7
newtef12 <newtef398@gmail.com>
2024-03-19 13:54:13 -0700

    mm

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,98 +1,53 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * reverse_list - Reverse a linked list
- * @head_ref: Pointer to pointer to the head of the list
- * Return: Pointer to the new head of the reversed list
- */
-listint_t *reverse_list(listint_t **head_ref)
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *prev = NULL;
-    listint_t *current = *head_ref;
-    listint_t *next = NULL;
+	listint_t *new;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-
-    *head_ref = prev;
-    return prev;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
-
 /**
- * compare_lists - Compare two linked lists
- * @head1: Pointer to the head of the first list
- * @head2: Pointer to the head of the second list
- * Return: 1 if lists are equal, else 0
- */
-int compare_lists(listint_t *head1, listint_t *head2)
-{
-    while (head1 != NULL && head2 != NULL)
-    {
-        if (head1->n != head2->n)
-            return 0;
-        head1 = head1->next;
-        head2 = head2->next;
-    }
-
-    // If both lists are NULL at this point, they are equal
-    return (head1 == NULL && head2 == NULL);
-}
-
-/**
- * is_palindrome - Identify if a singly linked list is a palindrome
- * @head: Pointer to pointer to the head of listint_t
- * Return: 1 if it is a palindrome, else 0
- */
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow_ptr = *head;
-    listint_t *fast_ptr = *head;
-    listint_t *prev_slow_ptr = *head;
-    listint_t *second_half = NULL;
-    listint_t *mid_node = NULL;
-    int is_pal = 1;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-
-    // Find the middle of the list using the slow and fast pointers
-    while (fast_ptr != NULL && fast_ptr->next != NULL)
-    {
-        fast_ptr = fast_ptr->next->next;
-        prev_slow_ptr = slow_ptr;
-        slow_ptr = slow_ptr->next;
-    }
-
-    // If the list has an odd number of elements, move the slow pointer to the next node
-    if (fast_ptr != NULL)
-    {
-        mid_node = slow_ptr;
-        slow_ptr = slow_ptr->next;
-    }
-
-    // Reverse the second half of the list
-    second_half = reverse_list(&slow_ptr);
-
-    // Compare the first and second halves of the list
-    is_pal = compare_lists(*head, second_half);
-
-    // Restore the original list by reversing the second half back and joining it with the first half
-    if (mid_node != NULL)
-    {
-        prev_slow_ptr->next = mid_node;
-        mid_node->next = second_half;
-    }
-    else
-    {
-        prev_slow_ptr->next = second_half;
-    }
-
-    return is_pal;
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
+	{
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
 }
