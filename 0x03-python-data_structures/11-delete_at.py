53cce018ac94437920f3130fb915e6277e3d8ce1
newtef12 <newtef398@gmail.com>
2024-03-19 05:58:03 -0700

    kk

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,7 +6,7 @@ def delete_at(my_list=[], idx=0):
     list_length = len(my_list)
 
     if idx >= list_length or list_length == 0:
-        return my_list[:]
+        return my_list
 
     new_list = my_list[:idx] + my_list[idx+1:]
     return new_list
