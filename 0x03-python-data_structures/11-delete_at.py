ccf64a5bd11a2285ed15c10237c67dbccca03ece
newtef12 <newtef398@gmail.com>
2024-03-19 05:47:03 -0700

    alx

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if not isinstance(my_list, list) or not isinstance(idx, int) or idx < 0:
+        return my_list
+
+    list_length = len(my_list)
+
+    if idx >= list_length:
+        return my_list
+
+    new_list = my_list[:idx] + my_list[idx+1:]
+    return new_list
