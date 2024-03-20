af517ec3a62ec99bdbbbc43e63068c57dcce2de7
newtef12 <newtef398@gmail.com>
2024-03-20 06:14:10 -0700

    mk

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,7 +6,13 @@
 
 def square_matrix_simple(matrix=[]):
     """Computes the square value of all integers of a matrix"""
-    return [[x ** 2 for x in row] for row in matrix]
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
 
 
 if __name__ == "__main__":
