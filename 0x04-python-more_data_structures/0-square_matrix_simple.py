da70b47efa5a1d91e343e2cccae6b309fffd7dcd
newtef12 <newtef398@gmail.com>
2024-03-20 06:18:49 -0700

    mk

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
-"""
-0-square_matrix_simple.py
-"""
-
-
 def square_matrix_simple(matrix=[]):
-    """Computes the square value of all integers of a matrix"""
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for num in row:
-            new_row.append(num ** 2)
-        new_matrix.append(new_row)
-    return new_matrix
+    new_matrix = matrix.copy()
 
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-
-    new_matrix = square_matrix_simple(matrix)
-    print(new_matrix)
-    print(matrix)
+    return (new_matrix)
