d3673edd06c254c13bd71d38ef262b8da6c44051
newtef12 <newtef398@gmail.com>
2024-03-20 08:13:12 -0700

    aa

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,30 +1,9 @@
 #!/usr/bin/python3
-"""
-102-complex_delete.py
-"""
-
-
 def complex_delete(a_dictionary, value):
-    """Deletes keys with a specific value in a dictionary"""
-    keys_to_delete = [key for key, val in a_dictionary.items() if val == value]
-    for key in keys_to_delete:
-        del a_dictionary[key]
-    return a_dictionary
+    list_keys = list(a_dictionary.keys())
 
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
 
-if __name__ == "__main__":
-    a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
-    new_dict = complex_delete(a_dictionary.copy(), 'C')
-    for key, value in sorted(a_dictionary.items()):
-        print("{}: {}".format(key, value))
-    print("--")
-    for key, value in sorted(new_dict.items()):
-        print("{}: {}".format(key, value))
-    print("--")
-    print("--")
-    new_dict = complex_delete(a_dictionary.copy(), 'c_is_fun')
-    for key, value in sorted(a_dictionary.items()):
-        print("{}: {}".format(key, value))
-    print("--")
-    for key, value in sorted(new_dict.items()):
-        print("{}: {}".format(key, value)))
+    return (a_dictionary)
