ca5f164a84ffd772c9d02f4070eb4f20c95521ba
newtef12 <newtef398@gmail.com>
2024-03-20 08:06:20 -0700

    qwe

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-    for key in sorted_keys:
-        print("{}: {}".format(key, a_dictionary[key]))
-
-# Test the function
-if __name__ == "__main__":
-    a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-    print_sorted_dictionary(a_dictionary)
+    x = sorted(a_dictionary)
+    for i in x:
+        print("{}: {}".format(i, a_dictionary[i]))
