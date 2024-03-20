4bb781543741e0b2888dc5661add8a345425f0b5
newtef12 <newtef398@gmail.com>
2024-03-20 07:59:12 -0700

    qw

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-"""11-multiply_list_map.py"""
-
 def multiply_list_map(my_list=[], number=0):
-    """Returns a new list with all values multiplied by a number using map"""
-    return list(map(lambda x: x * number, my_list))
+    return list(map((number).__mul__, my_list))
