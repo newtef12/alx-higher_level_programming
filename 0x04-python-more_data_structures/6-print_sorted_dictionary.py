4b7f256227ca227738b7e19891795fa775efa032
newtef12 <newtef398@gmail.com>
2024-03-20 07:47:25 -0700

    pp

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-"""
-6-print_sorted_dictionary.py
-"""
-
-
 def print_sorted_dictionary(a_dictionary):
-    """Prints a dictionary by ordered keys"""
-    sorted_keys = sorted(a_dictionary.keys())
-    for key in sorted_keys:
-        print("{}: {}".format(key, a_dictionary[key]))
-
-
-if __name__ == "__main__":
-    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
-    print_sorted_dictionary(a_dictionary)
+    x = sorted(a_dictionary)
+    for i in x:
+        print("{}: {}".format(i, a_dictionary[i]))
