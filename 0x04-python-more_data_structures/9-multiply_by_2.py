daaa12030edb3f7c58f747aad7c4b345729385cb
newtef12 <newtef398@gmail.com>
2024-03-20 08:02:43 -0700

    oo

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-"""
-9-multiply_by_2.py
-"""
-
-
 def multiply_by_2(a_dictionary):
-    """Returns a new dictionary with all values multiplied by 2"""
-    new_dict = {key: value * 2 for key, value in a_dictionary.items()}
-    return new_dict
-
-
-if __name__ == "__main__":
-    a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-    new_dict = multiply_by_2(a_dictionary)
-    for key, value in new_dict.items():
-        print("{}: {}".format(key, value)))
+    my_dict = dict(a_dictionary)
+    for k, v in my_dict.items():
+        my_dict[k] = v * 2
+    return my_dict
