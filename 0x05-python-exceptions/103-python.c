da5842fb793ce8220bdc3ba1baa25226f439267c
newtef12 <newtef398@gmail.com>
2024-04-01 10:29:40 -0700

    pp

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -15,33 +15,33 @@ void print_python_float(PyObject *p);
  */
 void print_python_list(PyObject *p)
 {
-        Py_ssize_t size, alloc, i;
-        const char *type;
-        PyListObject *list = (PyListObject *)p;
-        PyVarObject *var = (PyVarObject *)p;
+    Py_ssize_t size, i;
+    const char *type;
+    PyListObject *list = (PyListObject *)p;
+    PyVarObject *var = (PyVarObject *)p;
 
-        size = var->ob_size;
-        alloc = list->allocated;
+    size = var->ob_size;
 
-        fflush(stdout);
+    fflush(stdout);
 
-        printf("[*] Python list info\n");
-        if (strcmp(p->ob_type->tp_name, "list") != 0)
-        {
-                printf("  [ERROR] Invalid List Object\n");
-                return;
-        }
+    printf("[*] Python list info\n");
+    if (!PyList_Check(p))
+    {
+        printf("  [ERROR] Invalid List Object\n");
+        return;
+    }
 
-        printf("[*] Size of the Python List = %ld\n", size);
-        printf("[*] Allocated = %ld\n", alloc);
+    printf("[*] Size of the Python List = %ld\n", size);
 
-        for (i = 0; i < size; i++)
-        {
-                type = list->ob_item[i]->ob_type->tp_name;
-                printf("Element %ld: %s\n", i, type);
-                if (strcmp(type, "bytes") == 0)
-                        print_python_bytes(list->ob_item[i]);
-                else if (strcmp(type, "float") == 0)
-                        print_python_float(list->ob_item[i]);
-        }
+    printf("[*] Allocated = %ld\n", list->allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        type = Py_TYPE(list->ob_item[i])->tp_name;
+        printf("Element %ld: %s\n", i, type);
+        if (strcmp(type, "bytes") == 0)
+            print_python_bytes(list->ob_item[i]);
+        else if (strcmp(type, "float") == 0)
+            print_python_float(list->ob_item[i]);
+    }
 }
