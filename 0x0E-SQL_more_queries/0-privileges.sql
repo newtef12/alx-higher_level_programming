5be88057691404ec0a32ad393232ae742e5176dc
newtef12 <newtef398@gmail.com>
2024-05-22 10:48:14 -0700

    aa

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,13 +1,4 @@
--- Create the users if they don't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
--- Grant some privileges to the users for demonstration purposes
-GRANT SELECT, INSERT, UPDATE ON *.* TO 'user_0d_1'@'localhost';
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
-
--- Show privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
